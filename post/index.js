1a950ac3a58cec603b02e91f55c50e6ee23ebb91
awellsbiz <awellsbiz@gmail.com>
2024-01-13 13:36:38 -0800

    updates

--- post/index.js
+++ post/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,13 +14,27 @@ app.get('/posts', (req,res) => {
     res.send(post)
 })
 
-app.post('/post', (req,res) => {
+app.post('/posts', async (req,res) => {
     const id = randomBytes(4).toString('hex')
     const { title } = req.body
     post[id] = {
         id, title
     }
+    
+    //posting to the event bus 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
     res.status(201).send(post[id])
+})
+
+app.post('/events', (req,res) => {
+    console.log('Recieved Event', req.body.type)
+
+    res.send({})
 })
 
 app.listen(4000, () => {
